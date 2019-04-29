9d6b232ca37224ab8c71ccd4fca441905a4eb491
Kelsey Low <kklowlow@gmail.com>
2019-04-29 19:16:18 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,14 @@ class App < Sinatra::Base
     end
     
     post '/teams'do 
-      
+      @team = Team.new(params[:pirate])
+
+      params[:hero][:teams].each do |ship_data|
+        Ship.new(ship_data)
+      end
+
+      @ships = Ship.all
+      erb :'pirates/show'
     end
 
 end
