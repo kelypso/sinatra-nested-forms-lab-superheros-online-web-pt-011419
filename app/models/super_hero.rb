d6c87df63766f46fd1e2138f848ea0373bfe2087
Kelsey Low <kklowlow@gmail.com>
2019-04-29 19:33:22 +0000

    Automatically backed up by Learn

--- app/models/super_hero.rb
+++ app/models/super_hero.rb
@@ -1,16 +1,15 @@
 class SuperHero
-  attr_accessor :name, :power, :bio 
-  
+  attr_accessor :name, :power, :bio
   @@super_heroes = []
-  
+
   def initialize(hero_data)
     @name = hero_data[:name]
     @power = hero_data[:power]
     @bio = hero_data[:bio]
     @@super_heroes << self
   end
-  
-  def self.all 
+
+  def self.all
     @@super_heroes
-  end 
+  end
 end
